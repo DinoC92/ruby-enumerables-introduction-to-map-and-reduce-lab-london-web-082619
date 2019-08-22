13fa18119387e2868600779d62f0fd183492035a
Dean Burgess <dinocastiglione@ymail.com>
2019-08-22 16:46:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,26 @@
 # My Code here....
+=begin
+arr = [10,20,30,40,50]
+
+
+def map_to_negativize(source_array)
+  array = [-10, -20, -30, -40, -50]
+  i = 5
+  while 5 < 5 do
+    array.push( -50)
+    i += 1
+  end
+  return array
+end 
+=end
+
 def map_to_negativize(source_array)
   array = []
   i = 0 
   while i < source_array.length do
-   array.push( source_array[i] *-1)
-   i += 1
-end
+  array.push( source_array[i] *-1)
+  i += 1
+  end
   return array
 end 
 
@@ -39,11 +54,36 @@ end
   return array
 end
 
-def reduce_to_total(source_array, starting_point)
-  array = []
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
   i = 0 
-  while i < source_array do 
-    array.push (source_array +)
+  while i < source_array.length do 
+    total += source_array[i]
     i += 1 
-  end 
-end  
+end 
+  return total
+end
+
+
+
+
+
+
+# Take an array of numbers and return the sum of all of them
+def reduce_to_total(numbers)
+  # Create a variable called total to keep track of the current total
+  total = 0
+  # Iterate through the array and for each number, add it to the current total
+  numbers.each do |number|
+    number
+  end
+  
+end
+
+
+# Return the total, which should be the sum of all the numbers
+
+
+
+
+
